691f35d4352da382f945cf8e9a3b3c7d2d3af290
Ryo Nakao <nakabonne@gmail.com>
2019-08-05 15:10:20 +0900

    wrap

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import "fmt"
 
 // heyhey
 func main() {
-	// TODO: Add more option b
-	fmt.Println("vim-go")
+	func() {
+		// TODO: Add more option b
+		fmt.Println("vim-go")
 
-	TODO := ""
-	fmt.Println(TODO)
-	fmt.Println("remove")
-	fmt.Println("oi")
+		TODO := ""
+		fmt.Println(TODO)
+		fmt.Println("remove")
+		fmt.Println("oi")
+	}()
 }
