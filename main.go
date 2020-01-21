224de80234edb1b8d7c0e6a1eac430a2093befb4
nakabonne <nakabonne@gmail.com>
2020-01-21 11:40:41 +0900

    Add foo

--- main.go
+++ main.go
@@ -32,3 +32,36 @@ func Hey() { // 22
 		}
 	}
 }
+
+func Foo() { // 23
+	var b1, b2, b3, b4 bool
+
+	if b1 { // +1
+		if b2 { // +2
+		} else {
+			if b3 { // +3
+			}
+		}
+	}
+
+	if b1 { // +1
+		if b2 { // +2
+		} else if b3 {
+			if b4 { // +3
+			}
+		}
+	}
+
+	if b1 { // +1
+		if b2 { // +2
+			if b3 { // +3
+				if b4 { // +4
+				}
+			}
+		}
+	}
+
+	if b1 { // +1
+	} else {
+	}
+}
