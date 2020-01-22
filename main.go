1d37df16997b284f3533b47500dc3ed701291f99
nakabonne <nakabonne@gmail.com>
2020-01-22 10:16:51 +0900

    Add bar

--- main.go
+++ main.go
@@ -1,88 +1,28 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 func main() {
-	Hey()
+	_ = bar()
+	_ = baz("baz")
+	_ = foo()
 }
 
-var b1, b2, b3, b4 bool
-
-func Hey() { // 22
-
-	if b1 { // +1
-		if b2 { // +2
-		} else {
-			if b3 { // +3
-			}
-		}
-	}
-
-	if b1 { // +1
-		if b2 { // +2
-		} else if b3 {
-			if b4 { // +3
-			}
-		}
-	}
-
-	if b1 { // +1
-		if b2 { // +2
-			if b3 { // +3
-				if b4 { // +4
-				}
-			}
-		}
-	}
+type Bar struct {
 }
 
-func Foo() { // 23
-	if b1 { // +1
-		if b2 { // +2
-		} else {
-			if b3 { // +3
-			}
-		}
-	}
-
-	if b1 { // +1
-		if b2 { // +2
-		} else if b3 {
-			if b4 { // +3
-			}
-		}
-	}
-
-	if b1 { // +1
-		if b2 { // +2
-			if b3 { // +3
-				if b4 { // +4
-				}
-			}
-		}
-	}
-
-	if b1 { // +1
-	} else {
-	}
+func bar() error {
+	return nil
 }
 
-// elseを加算すると仮定
-func Bar() { // 21
-	if b1 { // +1
-		if b2 { // +2
-			if b3 { // +3
-			} else if b4 { // +1
-			} else { // +1
-			}
-		}
-	} else { // +1
-		if b1 { // +2
-			if b2 { // +3
-			} else { // +1
-				if b3 { // +4
-				} else if b4 { // +1
-				} else { // +1
-				}
-			}
-		}
-	}
+func baz(s string) string {
+	return ""
+}
+
+func foo() error {
+	msg := "foo"
+	return errors.New(fmt.Sprintf("error: %s", msg))
 }
